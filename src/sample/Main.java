ba9aa400e273fd4388912249537cf82218395b12
Morgan Norwood <morgannorwood@gmail.com>
2017-11-20 11:33:40 -0500

    assignment 15.11

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,48 +1,58 @@
 /*
 Morgan Norwood
 11/20/2017
-Program will display the message "java is fun",
-when the user clicks on the text, the message
-displays the text "java is powerful"
+A program that moves a circle up, down, left,
+or right using the arrow keys.
+
  */
 
-
 package sample;
+
 import javafx.application.Application;
-import javafx.collections.ObservableList;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
-import javafx.scene.text.Text;
+import javafx.scene.shape.Circle;
+import javafx.scene.layout.Pane;
+import javafx.geometry.Insets;
 import javafx.stage.Stage;
-import javafx.scene.paint.Color;
 
 public class Main extends Application {
 
     @Override
     public void start (Stage primaryStage) {
-    StackPane pane = new StackPane();
-    Text text1 = new Text("Java is fun");
-    Text text2 = new Text("Java is powerful");
-		pane.getChildren().addAll(text1);
+        // create pane
+        Pane pane = new Pane();
+        pane.setPadding(new Insets(20, 20, 20, 20));
+        // new circle
+        Circle circle = new Circle(20, 20, 20);
+        pane.getChildren().add(circle);
 
-		pane.setOnMouseClicked(e -> {
-        if (pane.getChildren().contains(text1)) {
-            pane.getChildren().add(text2);
-            pane.getChildren().remove(text1);
-        }
-        else {
-            pane.getChildren().add(text1);
-            pane.getChildren().remove(text2);
-        }
-    });
 
-    // Create window 
-    Scene scene = new Scene(pane, 400, 400);
-		primaryStage.setTitle("Assignment15"); // Set the title
-		primaryStage.setScene(scene); //set scene
-		primaryStage.show(); // Display 
-}
+        pane.setOnKeyPressed(a -> {
+            switch (a.getCode()) {
+                case UP : circle.setCenterY(circle.getCenterY() >
+                        circle.getRadius() ? circle.getCenterY() - 10 :
+                        circle.getCenterY()); break;
+                case DOWN : circle.setCenterY(circle.getCenterY() <
+                        pane.getHeight() - circle.getRadius() ?
+                        circle.getCenterY() + 10 : circle.getCenterY());
+                    break;
+                case LEFT : circle.setCenterX(circle.getCenterX() >
+                        circle.getRadius() ? circle.getCenterX() - 10 :
+                        circle.getCenterX()); break;
+                case RIGHT : circle.setCenterX(circle.getCenterX() <
+                        pane.getWidth() - circle.getRadius() ?
+                        circle.getCenterX() + 10: circle.getCenterX());
+            }
+        });
 
+        // Create a scene and place it in the stage
+        Scene scene = new Scene(pane, 200, 200);
+        primaryStage.setTitle("Assignment15"); // Set the stage title
+        primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.show(); // Display the stage
+
+        pane.requestFocus();
+    }
     public static void main(String[] args) {
         launch(args);
     }
